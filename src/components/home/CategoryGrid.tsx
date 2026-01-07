bc17bdc32993dd2aacf7072cc27c6df19c4decd9
daniyal0302 <133259300+daniyal0302@users.noreply.github.com>
2026-01-07 21:15:38 +0500

    Adjust grid layout

--- src/components/home/CategoryGrid.tsx
+++ src/components/home/CategoryGrid.tsx
@@ -219,7 +219,7 @@ const CategoryGrid = () => {
           </p>
         </div>
 
-        <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 md:gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 md:gap-8">
           {categories.map((category, index) => {
             const categoryImage = categoryImages[category.slug];
 
